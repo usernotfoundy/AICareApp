e26167023ef9e497d4feebd503e60390335bc789
Emzkie The Great <63085286+hwangxiaoman@users.noreply.github.com>
2023-04-24 15:04:43 +0800

    GET STARTED
    pages 0-3

--- AICare/App.js
+++ AICare/App.js
@@ -1,13 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  Button,
+  Pressable
+} from 'react-native';
 
-export default function App() {
+export default function App(props) {
+
+  const { onPress, buttonTitle = 'Get Started' } = props;
+
   return (
-    <View style={styles.container}>
-      <Image source={require('./assets/AICareAppLogo.png')} style={styles.setImage} />
-      <Text style={styles.setTextColor}>CARE</Text>
+    <SafeAreaView style={styles.container}>
+      <Image source={require('./assets/pickUp_getStarted.png')} style={styles.setImage} fadeDuration={1000} />
+
+      <Text style={styles.setText1}>Get the Fastest Delivery Services for you.</Text>
+
+      <Text style={styles.setText2}>We ensure that the items you require arrive on time. You focus on what needs to be done.</Text>
+
+      <Pressable style={styles.setButtonStyle} onPress={onPress}>
+        <Text style={styles.setRegularText}>{buttonTitle}</Text>
+      </Pressable>
+
+      <Text style={styles.setText2}>3/3</Text>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
 
   );
 }
@@ -15,19 +37,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ecfdf5',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 100,
   },
-  setTextColor: {
+  setText1: {
     color: '#065F46',
-    fontSize: 50,
-    margin: 0,
+    fontSize: 27,
+    marginTop: 50,
     padding: 0,
     fontWeight: 900,
+    width: 300,
+    textAlign: 'center',
+  },
+  setText2: {
+    color: '#94a3b8',
+    fontSize: 15,
+    marginTop: 20,
+    padding: 0,
+    width: 290,
+    textAlign: 'center',
   },
   setImage: {
     height: 200,
     width: 200,
+  },
+  setButtonStyle: {
+    height: 55,
+    width: 250,
+    backgroundColor: '#047857',
+    justifyContent: 'center',
+    marginTop: 50,
+    borderRadius: 50,
+
+  },
+  setRegularText: {
+    color: '#ffffff',
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: 700,
   },
 });
